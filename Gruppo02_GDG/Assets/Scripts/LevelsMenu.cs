a8c2cf78b00eb626f69284d5344304a4c59812f3
FeGio <57982988+FeGio@users.noreply.github.com>
2020-06-04 12:44:36 +0200

    Fè_12:44_Start+LvlMenu

---
+++ Gruppo02_GDG/Assets/Scripts/LevelsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelsMenu : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackToStartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
